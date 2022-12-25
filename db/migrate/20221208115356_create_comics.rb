64e33be7f6ef34ba0c0e5992c7c0f147b8d25177
j-shigeoka <114391855+j-shigeoka@users.noreply.github.com>
2022-12-25 09:32:45 +0000

    fix migration file

--- db/migrate/20221208115356_create_comics.rb
+++ db/migrate/20221208115356_create_comics.rb
@@ -1,8 +1,8 @@
 class CreateComics < ActiveRecord::Migration[6.1]
   def change
     create_table :comics do |t|
-      t.integer :genre_id,          null: false, default: ""
-      t.integer :user_id,           null: false, default: ""
+      t.integer :genre_id,          null: false
+      t.integer :user_id,           null: false
       t.string  :name,              null: false, default: ""
       t.string  :introduction,      null: false, default: ""
       t.timestamps
