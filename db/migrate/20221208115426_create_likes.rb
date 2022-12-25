ea30381713ff5ed54134f92c2bb2fb890add132d
j-shigeoka <114391855+j-shigeoka@users.noreply.github.com>
2022-12-25 09:36:55 +0000

    fix migration file

--- db/migrate/20221208115426_create_likes.rb
+++ db/migrate/20221208115426_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
-      t.integer  :user_id,     null: false, default: ""
-      t.integer  :comic_id,    null: false, default: ""
+      t.integer  :user_id,     null: false
+      t.integer  :comic_id,    null: false
       t.timestamps
     end
   end
