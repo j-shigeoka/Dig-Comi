c0990d3cda00895dbc833fb05240ef7d4631d413
j-shigeoka <114391855+j-shigeoka@users.noreply.github.com>
2023-01-07 06:19:07 +0000

    [Add]レイアウト

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,6 @@ class Public::HomesController < ApplicationController
   def top
     @user = current_user
     @ranks = Comic.find(Like.group(:comic_id).order('count(comic_id) desc').limit(5).pluck(:comic_id))
-    @new_comics = Comic.all.order(created_at: :desc).page(params[:page]).per(16)
+    @new_comics = Comic.all.order(created_at: :desc).page(params[:page]).per(1)
   end
 end
